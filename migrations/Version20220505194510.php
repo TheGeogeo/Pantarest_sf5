6f85c31b090097fe374d064a4eb7aff36a219662
TheGeogeo <coudrier.g@gmail.com>
2022-05-05 21:51:57 +0200

    reinstall

--- migrations/Version20220505194510.php
+++ migrations/Version20220505194510.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220425173009 extends AbstractMigration
+final class Version20220505194510 extends AbstractMigration
 {
     public function getDescription(): string
     {
