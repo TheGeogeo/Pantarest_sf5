fbdde18b91c4e8adedd84f0f31443316f9e980ad
TheGeogeo <coudrier.g@gmail.com>
2022-05-19 21:20:37 +0200

    Add user make user + add lastName & firstName

--- migrations/Version20220519191934.php
+++ migrations/Version20220519191934.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220505195944 extends AbstractMigration
+final class Version20220519191934 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add image';
+        return '';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE pin ADD image_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE users ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE pin DROP image_name');
+        $this->addSql('ALTER TABLE users DROP first_name, DROP last_name');
     }
 }
