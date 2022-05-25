0a77f9819b84beda1995e7aa44d7c9d7eb81194b
TheGeogeo <coudrier.g@gmail.com>
2022-05-25 16:11:05 +0200

    add user for pin + add method getFullName

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -188,4 +188,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullName(): string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
 }
