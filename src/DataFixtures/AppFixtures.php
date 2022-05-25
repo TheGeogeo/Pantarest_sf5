3e07f16b18b1e209acec2f141606a3a22ea84ac5
TheGeogeo <coudrier.g@gmail.com>
2022-05-25 16:00:21 +0200

    fixture pin + user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Pin;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,12 +11,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 5; $i++) {
-            $pin = new Pin();
-            $pin->setTitle("Title $i");
-            $pin->setDescription("Description $i");
-            $manager->persist($pin);
-        }
+        $user = new User();
+        $user->setFirstName("Geogeo");
+        $user->setLastName("Lamen");
+        $user->setEmail("geogeo@example.com");
+        $user->setPassword("$2y$13$0riMY0maGqmtgts9pyS7JOvAHEq3x0Q3qHsLS7xIMNHT8CXmNb2C6"); //geogeo
+        $manager->persist($user);
+
+        $pin = new Pin();
+        $pin->setTitle("pin1");
+        $pin->setDescription("la description pin 1");
+        $pin->setUser($user);
+        $manager->persist($pin);
 
         $manager->flush();
     }
